532559a1a6e750bc9b5a82119c6bf00baf7cb015
wingillis <win.gillis@gmail.com>
2017-12-04 20:34:57 -0500

    Proper blue template

--- highlight_finder.py
+++ highlight_finder.py
@@ -10,15 +10,19 @@ from skimage.measure import label, regionprops
 
 def main(fname):
     templates = dict(yellow=np.array([251, 240, 187]),
-                     blue=np.array([192, 240, 246]))
+                     blue=np.array([210, 255, 245]))
     im = imread(fname)
     if im.shape[2] > 3:
         im = rgba2rgb(im)*255
     im_files = []
     for key, template in templates.items():
+        print('Testing {}'.format(key))
         regions = get_highlighted_regions(im, template)
         if regions:
+            print('Found {} highlights'.format(len(regions)))
             im_files += save_highlight_extract(im, regions, key, fname)
+        else:
+            print('No highlights found for this color')
 
     return im_files
 
@@ -33,7 +37,8 @@ def save_highlight_extract(im, bounds, color, fname):
         imsave(os.path.join(path, f), tmp)
     return fnames
 
-def get_highlighted_regions(im, color, threshold=25):
+def get_highlighted_regions(im, color, threshold=12):
+    disk_size = 7
     # reshape to 2d matrix
     im_tmp = im.reshape((im.shape[0]*im.shape[1], im.shape[2]))
     template = np.repeat(color, im_tmp.shape[0]).reshape((3, -1)).T
@@ -42,15 +47,14 @@ def get_highlighted_regions(im, color, threshold=25):
     # calculate euclidean distance of resulting color vector
     euclidean = norm(dist, axis=1)
     # find places of minimum value
-    m = mode(euclidean[euclidean<threshold]).mode
+    m = stats.mode(euclidean[euclidean<threshold]).mode
     if m.size != 0:
         mask = (euclidean==m).reshape(im.shape[:2])
-        mask = binary_dilation(mask, disk(7))
+        mask = binary_dilation(mask, disk(disk_size))
         labeled = label(mask)
         props = regionprops(labeled)
-        return [region.bbox for region in props]
+        return [region.bbox for region in props if region.minor_axis_length > disk_size*np.pi]
     else:
-        print('No highlights found for this color')
         return []
 
 if __name__=='__main__':
