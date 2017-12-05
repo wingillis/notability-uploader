97230af4ea13f3b6268d6df8a9104bb60608957d
wingillis <win.gillis@gmail.com>
2017-12-04 20:17:15 -0500

    new algo written

--- highlight_finder.py
+++ highlight_finder.py
@@ -2,13 +2,14 @@ import sys
 import os
 import numpy as np
 import scipy.stats as stats
+from numpy.linalg import norm
 from skimage.io import imread, imsave
 from skimage.color import rgb2gray, rgba2rgb
 from skimage.morphology import binary_dilation, disk
 from skimage.measure import label, regionprops
 
 def main(fname):
-    templates = dict(yellow=np.array([250, 239, 190]),
+    templates = dict(yellow=np.array([251, 240, 187]),
                      blue=np.array([192, 240, 246]))
     im = imread(fname)
     if im.shape[2] > 3:
@@ -16,7 +17,8 @@ def main(fname):
     im_files = []
     for key, template in templates.items():
         regions = get_highlighted_regions(im, template)
-        im_files += save_highlight_extract(im, regions, key, fname)
+        if regions:
+            im_files += save_highlight_extract(im, regions, key, fname)
 
     return im_files
 
@@ -31,15 +33,25 @@ def save_highlight_extract(im, bounds, color, fname):
         imsave(os.path.join(path, f), tmp)
     return fnames
 
-def get_highlighted_regions(im, color):
-    im_tmp = im.copy()
-    for i in range(im.shape[0]):
-        for j in range(im.shape[1]):
-            im_tmp[i, j, :] = im_tmp[i, j, :] - color
-    blobs = binary_dilation(rgb2gray(im_tmp) < 0.01, disk(5))
-    labeled = label(blobs)
-    props = regionprops(labeled)
-    return [region.bbox for region in props]
+def get_highlighted_regions(im, color, threshold=25):
+    # reshape to 2d matrix
+    im_tmp = im.reshape((im.shape[0]*im.shape[1], im.shape[2]))
+    template = np.repeat(color, im_tmp.shape[0]).reshape((3, -1)).T
+    # subtract the template from the image
+    dist = im_tmp - template
+    # calculate euclidean distance of resulting color vector
+    euclidean = norm(dist, axis=1)
+    # find places of minimum value
+    m = mode(euclidean[euclidean<threshold]).mode
+    if m.size != 0:
+        mask = (euclidean==m).reshape(im.shape[:2])
+        mask = binary_dilation(mask, disk(7))
+        labeled = label(mask)
+        props = regionprops(labeled)
+        return [region.bbox for region in props]
+    else:
+        print('No highlights found for this color')
+        return []
 
 if __name__=='__main__':
     main(sys.argv[1])
